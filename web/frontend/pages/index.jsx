700acfa83884815603ce4e2c2fbe714fb216804b
Ahyas Widyatmaka <ahyasw@gmail.com>
2023-07-27 00:20:43 +0900

    3rd test, changing wellcome page message

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -32,7 +32,7 @@ export default function HomePage() {
               <Stack.Item fill>
                 <TextContainer spacing="loose">
                   <Text as="h2" variant="headingMd">
-                    Welcome to Simple Finance App by Askara Solutions
+                    Welcome to Simple Finance App by Ahyas Widyatmaka
                   </Text>
                   <p>
                     <Trans

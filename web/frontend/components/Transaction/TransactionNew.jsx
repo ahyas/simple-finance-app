4a4ecd5d99f4882836ac7213fc9e9c3359d1e5c9
Ahyas Widyatmaka <ahyasw@gmail.com>
2023-07-30 16:21:49 +0900

    Running test 2

--- web/frontend/components/Transaction/TransactionNew.jsx
+++ web/frontend/components/Transaction/TransactionNew.jsx
@@ -1,9 +1,58 @@
 import { LegacyCard, Page, Layout, Form, FormLayout, TextField, Button, Select, DatePicker } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
+import {useForm, useField} from "@shopify/react-form";
 import { useCallback } from "react";
  
 export function TransactionNew(){
-    
+    const {
+        fields:{
+            category,
+            sub_category,
+            date,
+            value, 
+            information
+        },
+        submit
+    } = useForm({
+        fields: {
+            category: useField({
+                value:0,
+                validates: (category)=>{
+                    if(category === 0){
+                        return "Choose category";
+                    }
+                }
+            }),
+            sub_category: useField({
+                value:0,
+                validates:(sub_category)=>{
+                    if(sub_category === 0){
+                        return "Choose sub category";
+                    }
+                }
+            }),
+            date: useField({
+                value:"",
+                validates:(date)=>{
+                    if(date === ""){
+                        return "Insert relevant date";
+                    }
+                }
+            }),
+            value: useField({
+                value:0,
+                validates:(value)=>{
+                    if(value === 0){
+                        return "Insert value";
+                    }
+                }
+            })
+        },
+        onSubmit: useCallback(
+            (form)=>{
+                console.log(form);    
+            },[])
+    });
 
     return(
         <Page 
@@ -19,7 +68,38 @@ export function TransactionNew(){
             <Layout>
                 <Layout.Section>
                 <LegacyCard sectioned>
-                <p>Add new transaction here</p>
+                <Form>
+                    <FormLayout>  
+                        <TextField 
+                            label="Category" 
+                            onChange={() => {}} 
+                            autoComplete="off" 
+                        />
+                        <TextField
+                            label="Sub category"
+                            onChange={() => {}}
+                            autoComplete="off"
+                        />
+                        <TextField
+                            label="Date"
+                            onChange={() => {}}
+                            autoComplete="off"
+                        />
+                        <TextField
+                            type="number"
+                            label="Value"
+                            onChange={() => {}}
+                            autoComplete="off"
+                            min={0}
+                        />
+                        <TextField
+                            label="Information"
+                            onChange={() => {}}
+                            autoComplete="off"
+                        />
+                        <Button onClick={submit} primary fullWidth>Save</Button>
+                    </FormLayout>
+                    </Form>
                 </LegacyCard>
                 </Layout.Section>
             </Layout>
